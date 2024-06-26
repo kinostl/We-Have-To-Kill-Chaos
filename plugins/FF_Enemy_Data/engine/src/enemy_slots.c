adc8e16f0634f6e9964d6a6936330d992e05f94a
Zone Booth <kinostl@gmail.com>
2024-06-26 01:29:48 -0400

    It should also free stuff

--- plugins/FF_Enemy_Data/engine/src/enemy_slots.c
+++ plugins/FF_Enemy_Data/engine/src/enemy_slots.c
@@ -3,6 +3,7 @@
 #include "data/max_global_vars.h"
 #include "enemy_data.h"
 #include "vm.h"
+#include "math.h"
 #include <gb/crash_handler.h>
 BANKREF(ff_enemy_slots)
 
@@ -11,11 +12,20 @@ WORD end_of_globals = MAX_GLOBAL_VARS;
 void* extra_reserve(SCRIPT_CTX* THIS, WORD size) OLDCALL BANKED
 {
     void * end_of = VM_REF_TO_PTR(end_of_globals + 1);
-    end_of_globals += (size/16);
+    end_of_globals = end_of_globals + DIV_16(size);
     if(end_of_globals > 512){
         __HandleCrash();
     }
     return end_of;
+}
+
+void extra_free(SCRIPT_CTX* THIS, WORD size) OLDCALL BANKED
+{
+    THIS;
+    end_of_globals = end_of_globals - DIV_16(size);
+    if(end_of_globals < MAX_GLOBAL_VARS){
+        end_of_globals = MAX_GLOBAL_VARS;
+    }
 }
 
 void handleEnemySlots(SCRIPT_CTX* THIS) OLDCALL BANKED
@@ -24,4 +34,9 @@ void handleEnemySlots(SCRIPT_CTX* THIS) OLDCALL BANKED
     enemy_slots[0] = enemy_db[IMP];
     enemy_slots[1] = enemy_db[WOLF];
     enemy_slots[2] = enemy_db[MADPONY];
+}
+
+void finishBattle(SCRIPT_CTX* THIS) OLDCALL BANKED
+{
+    extra_free(THIS, sizeof(struct enemy_info) * 6);
 }
