8fabe2c6efde0cd5f1a5f23314ec41dae4e6f4a8
Zone Booth <kinostl@gmail.com>
2024-08-12 19:01:32 -0400

    Combine armor and weapon stores, add other stores, and clinic

---
+++ maps/shed_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="shed_tileset" tilewidth="8" tileheight="8" tilecount="322" columns="14">
+ <image source="shed_tileset.png" width="112" height="185"/>
+</tileset>
