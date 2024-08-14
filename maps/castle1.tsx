a3b2af8edc00b31466366010490aeaf2b67a0c1a
Zone Booth <kinostl@gmail.com>
2024-08-14 02:39:16 -0400

    But what if instead Cornelia castle was based on Bodiam Castle.

---
+++ maps/castle1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="castle1" tilewidth="8" tileheight="8" tilecount="3599" columns="59">
+ <image source="castle1.png" width="472" height="488"/>
+</tileset>
---
+++ maps/castle2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="castle2" tilewidth="8" tileheight="8" tilecount="360" columns="18">
+ <image source="castle2.png" width="144" height="160"/>
+</tileset>
