e45bcfd3b055f7a97963fd1df013b42266669f56
Zone Booth <kinostl@gmail.com>
2024-08-16 15:25:01 -0400

    I think thats all of Cornelia connected

---
+++ maps/corneria.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="corneria" tilewidth="8" tileheight="8" tilecount="3072" columns="64">
+ <image source="../assets/backgrounds/corneria.png" width="512" height="384"/>
+</tileset>
