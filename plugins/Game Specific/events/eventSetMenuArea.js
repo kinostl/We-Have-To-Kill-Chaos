6abe329b158f97320795db8c41e05416e8a668b4
Zone Booth <kinostl@gmail.com>
2024-08-18 20:51:03 -0400

    Add load menu value stuff

--- plugins/Game Specific/events/eventSetMenuArea.js
+++ plugins/Game Specific/events/eventSetMenuArea.js
@@ -19,10 +19,18 @@ const fields = [
                 key: "max",
                 type: "number",
                 label: "Max",
-                defaultValue: "0"
+                defaultValue: "1",
+                min: "1"
             }
 
         ]
+    },
+    {
+        key: "load_val",
+        type: "value",
+        label: "Load Value as Selection",
+        defaultType: "number",
+        defaultValue: "1"
     }
 ]
 
@@ -30,14 +38,25 @@ const compile = (input, helpers) => {
     const {
         actorSetById,
         actorSetPosition,
+        _setMemInt8ToVariable,
         _setConstMemInt8,
-        _setConstMemInt16
+        _setConstMemInt16,
+        variableSetToScriptValue,
+        variableValueOperation,
+        _declareLocal,
+        _markLocalUse
     } = helpers;
+    const valHolder = _declareLocal(`fs_menu_min_sel`, 1, true);
+    variableSetToScriptValue(valHolder, input.load_val);
+    variableValueOperation(valHolder, ".MAX", 1);
+
     actorSetById("player")
     actorSetPosition(input.x, input.y);
     _setConstMemInt16("cursor_base_pos", ((input.y * 16 * 8) >> 7) << 7);
-    _setConstMemInt8("menu_selection", 1);
+    _setMemInt8ToVariable("menu_selection", valHolder);
     _setConstMemInt8("max_menu_selection", input.max);
+
+    _markLocalUse(valHolder)
 };
 
 module.exports = {
