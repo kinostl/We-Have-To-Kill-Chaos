1d017023f83a595031f5970f011aee40fd30294a
Zone Booth <kinostl@gmail.com>
2024-11-07 23:34:52 -0500

    We're worshipping at the combination holy site water purification temple

---
+++ maps/temple_of_fiends.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="temple_of_fiends" tilewidth="8" tileheight="8" tilecount="256" columns="16">
+ <image source="../tilesets2/temple_of_fiends.png" width="128" height="128"/>
+</tileset>
