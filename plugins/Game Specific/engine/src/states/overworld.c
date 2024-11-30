1f6b410fe5898e0c7271dfb297dd6111b0f5f4e2
Zone Booth <kinostl@gmail.com>
2024-11-30 02:59:05 -0500

    Lets play dynamic overworld map game

--- plugins/Game Specific/engine/src/states/overworld.c
+++ plugins/Game Specific/engine/src/states/overworld.c
@@ -6,6 +6,7 @@
 #include <bankdata.h>
 #include <camera.h>
 #include <collision.h>
+#include <game_time.h>
 #include <gb/cgb.h>
 #include <gb/gb.h>
 #include <gb/hardware.h>
@@ -14,13 +15,29 @@
 #include <interrupts.h>
 #include <scroll.h>
 #include <types.h>
+#include <actor.h>
 #pragma bank 255
 
 #include "overworld/overworld_tiles.h"
 #include "palette.h"
 #include "states/overworld.h"
+#include <math.h>
+
+void update_world(UBYTE map_x, UBYTE map_y) BANKED {
+    overworld_far_ptr overworld_ptr;
+    set_overworld_ptr(&overworld_ptr, map_x, map_y);
+
+    image_bank = overworld_ptr.bank;
+    image_ptr = overworld_ptr.map_ptr;
+
+    image_attr_bank = overworld_ptr.bank;
+    image_attr_ptr = overworld_ptr.attrs_ptr;
+
+    scroll_repaint();
+}
 
 void overworld_init(void) BANKED {
+  PLAYER.pos.y = PLAYER.pos.x = 4 << 7;
   SetBankedBkgData(0, overworld_tiles_TILE_COUNT, overworld_tiles_tiles,
                    BANK(overworld_tiles));
   MemcpyBanked(BkgPalette, overworld_tiles_palettes,
@@ -34,20 +51,36 @@ void overworld_init(void) BANKED {
   image_height = image_tile_height * 8;
   scroll_y_max = image_height - ((UINT16)SCREENHEIGHT);
 
+  update_world(0, 0);
 }
 
 void overworld_update(void) BANKED {
+  static UBYTE map_x = 0;
+  static UBYTE map_y = 0;
+
   topdown_update();
-  if (INPUT_A_OR_B_PRESSED) {
-    overworld_far_ptr overworld_ptr;
-    set_overworld_ptr(&overworld_ptr, 0, 0);
 
-    image_bank = overworld_ptr.bank;
-    image_ptr = overworld_ptr.map_ptr;
+  UBYTE next_map_x = map_x;
+  UBYTE next_map_y = map_y;
 
-    image_attr_bank = overworld_ptr.bank;
-    image_attr_ptr = overworld_ptr.attrs_ptr;
+  if ((PLAYER.pos.x >> 7) > 29) {
+    next_map_x++;
+    PLAYER.pos.x -= 29 << 7;
+  } else if ((PLAYER.pos.y >> 7) > 29) {
+    next_map_y++;
+    PLAYER.pos.y -= 29 << 7;
+  } else if ((PLAYER.pos.x >> 7) < 3) {
+    next_map_x--;
+    PLAYER.pos.x += 28 << 7;
+  } else if ((PLAYER.pos.y >> 7) < 3) {
+    next_map_y--;
+    PLAYER.pos.y += 28 << 7;
+  }
 
-    scroll_repaint();
+  if (map_x != next_map_x || map_y != next_map_y) {
+    map_x = next_map_x;
+    map_y = next_map_y;
+
+    update_world(map_x, map_y);
   }
 }
